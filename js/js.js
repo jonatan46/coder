11363ab79cfefafce99aa6cc5f5b496c0e7ee91a
jonatan.trevisan <jonatan.trevisan@addoc.com>
2021-07-14 20:38:54 -0300

    Subiendo Archivos Primera Version

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function mobile(){
+    document.getElementById("idMobile").classList.toggle("mobile");
+  } 
+  // alert("hola");
+    // var mobile = document.getElementsByTagName("ul").style.display = "block";
+    // document.getElementsByTagName("p");
+    // .style.display = "block";
+    // document.getElementById("myDiv").style.borderStyle = "solid";
